db56a4ecb43b26b2d109fccdb10ae50e2e5b59f1
Chan, Danny <danny50610@yahoo.com.tw>
2022-11-29 22:00:24 +0800

    整理 code

---
+++ packages/main/src/twitter.ts
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { addTweet } from './database';
+
+export async function fetchTwitterUserLiked() {
+  const userId = import.meta.env.VITE_USER_ID;
+
+  let count = 0;
+  const maxCount = 222;
+  let paginationToken = null;
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    const params: {[k: string]: any} = {
+      'tweet.fields': [
+        'created_at',
+        'author_id',
+        'entities',
+        'attachments',
+      ].join(','),
+      'media.fields': [
+        'duration_ms',
+        'height',
+        'media_key',
+        'preview_image_url',
+        'public_metrics',
+        'type',
+        'url',
+        'width',
+        'alt_text',
+      ].join(','),
+      'expansions': 'attachments.media_keys',
+    };
+
+    if (paginationToken != null) {
+      params.pagination_token = paginationToken;
+    }
+
+    const response = await axios.get(`https://api.twitter.com/2/users/${userId}/liked_tweets`, {
+      headers: {
+        'Authorization': 'Bearer ' + import.meta.env.VITE_TWITTER_TOKEN,
+      },
+      params,
+    });
+
+    const data = response.data;
+
+    if (data.data) {
+      data.data.forEach((tweet: any) => {
+        addTweet(tweet.id, tweet);
+      });
+    }
+
+    // TODO: data.includes.media
+
+    count += data.meta.result_count ?? 0;
+    if (count > maxCount) {
+      break;
+    }
+
+    if (data.meta.next_token === undefined) {
+      break;
+    } else {
+      paginationToken = data.meta.next_token;
+    }
+  }
+}
+
+// export async function test2() {
+//   addTweet('1111', {'AAA': 'BBB'});
+//   addTweet('1111', {'AAA': 'CCC'});
+//   addTweet('1112', {'AAA': 'BBB'});
+// }
--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -6,9 +6,10 @@
 
 // const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 
-// function fetchTwitterUserLiked() {
-//   window.electronAPI.fetchTwitterUserLiked();
-// }
+async function fetchTwitterUserLiked() {
+  await window.electronAPI.fetchTwitterUserLiked();
+  console.log('Done');
+}
 
 // function test2() {
 //   window.electronAPI.test2();
@@ -18,8 +19,13 @@
 
 <template>
   <div class="container py-4 px-3 mx-auto">
-    <h1>Hello, Bootstrap and Vite!</h1>
-    <button class="btn btn-primary">Primary button</button>
+    <h1>Twitter Backup</h1>
+    <button
+      class="btn btn-primary"
+      @click="fetchTwitterUserLiked"
+    >
+      手動抓取
+    </button>
   </div>
 </template>
 
